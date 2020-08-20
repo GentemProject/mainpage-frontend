a6417d5d38012e9602f65531bc3bab5ecb50fcdc
maticaba99 <maticaba99@hotmail.com>
2020-08-20 20:19:49 -0300

    yarn format

--- backend/src/api/schemas/project.ts
+++ backend/src/api/schemas/project.ts
@@ -49,30 +49,33 @@ const projectSchema = new Schema(
 const projectModel = mongoose.model('Project', projectSchema);
 export default projectModel;
 
-const oldSchema = new Schema({
-  slug: String,
-  name: String,
-  logo: String,
-  objective: String,
-  communityworkwith: [Number],
-  description: String,
-  howusedonations: String,
-  location: String,
-  sponsors: [String],
-  paymentslink: String,
-  accounts: String,
-  instructionstodeliverproducts: String,
-  website: String,
-  email: String,
-  phones: String,
-  whatsapp: String,
-  instagram: String,
-  facebook: String,
-  twitter: String,
-  linkedin: String,
-  city: String,
-  country: String,
-  createtAt: Number,
-}, {strict: false});
+const oldSchema = new Schema(
+  {
+    slug: String,
+    name: String,
+    logo: String,
+    objective: String,
+    communityworkwith: [Number],
+    description: String,
+    howusedonations: String,
+    location: String,
+    sponsors: [String],
+    paymentslink: String,
+    accounts: String,
+    instructionstodeliverproducts: String,
+    website: String,
+    email: String,
+    phones: String,
+    whatsapp: String,
+    instagram: String,
+    facebook: String,
+    twitter: String,
+    linkedin: String,
+    city: String,
+    country: String,
+    createtAt: Number,
+  },
+  { strict: false },
+);
 
 export const oldModel = mongoose.model('organiz', oldSchema, 'organiz');
