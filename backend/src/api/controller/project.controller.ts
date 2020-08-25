79c47d8572822749b1e58e5e92d7c83041c42150
maticaba99 <maticaba99@hotmail.com>
2020-08-25 07:44:50 -0300

    volviendo al cambio anterior

--- backend/src/api/controller/project.controller.ts
+++ backend/src/api/controller/project.controller.ts
@@ -67,7 +67,7 @@ const ProjectCtrl = {
         if (err) {
           return res.json(err);
         } else {
-          return res.json(result[0]);
+          return res.json(result);
         }
       },
     );
