04ba9f9593348fb54af1fa2e56e0c03daf4e4d6f
maticaba99 <maticaba99@hotmail.com>
2020-09-10 15:07:01 -0300

    pagination filter show cuantity per filter

--- backend/src/api/controller/paginationFilter.ts
+++ backend/src/api/controller/paginationFilter.ts
@@ -1,18 +1,18 @@
 export async function paginationFilter(filter, model, req, res) {
   let cuantity = await model.countDocuments(filter);
-  console.log(filter);
+  /*   console.log(filter); */
   try {
     const perPage: number = parseInt(req.query.limit as string);
-    console.log(perPage, 'perPage');
+    /*     console.log(perPage, 'perPage'); */
     const totalPages: number = Math.floor(cuantity / perPage);
-    console.log(totalPages, 'totalPages');
+    /*     console.log(totalPages, 'totalPages'); */
     let page: number = parseInt(req.query.skip as string);
-    console.log(page, 'page');
+    /*     console.log(page, 'page'); */
     if (page > totalPages) {
       res.end();
     }
     const show = page * perPage;
-    console.log(show, 'show');
+    /*     console.log(show, 'show'); */
 
     model
       .find(filter)
@@ -20,6 +20,7 @@ export async function paginationFilter(filter, model, req, res) {
       .limit(perPage)
       .then(data =>
         res.json({
+          totalOrg: cuantity,
           page: page,
           totalPages: totalPages,
           data: data,
