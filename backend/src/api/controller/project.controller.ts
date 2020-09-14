ab2b93cd31de7da758f2d298bca495540f7f1d83
maticaba99 <maticaba99@hotmail.com>
2020-09-14 19:34:17 -0300

    new orgs added

--- backend/src/api/controller/project.controller.ts
+++ backend/src/api/controller/project.controller.ts
@@ -26,9 +26,91 @@ const ProjectCtrl = {
   getPagination: async (req: Request, res: Response) => {
     await paginationFilter({}, projectModel, req, res);
   },
-
   createProject: async (req: Request, res: Response) => {
-    console.log(req.body);
+    /*     interface organization {
+          slug: {
+            type: string,
+          },
+          primaryData: {
+            name: {
+              type: string,
+              trim: true,
+            },
+            logo: string,
+            objective: string,
+            description: string,
+            howUseDonation: string,
+            sponsors: [string],
+            communityId: [number],
+          },
+          contact: {
+            email: string,
+            phone: string,
+            whatsapp: string,
+            instagram: string,
+            facebook: string,
+            linkedin: string,
+            twitter: string,
+            website: string,
+          },
+          paymentData: {
+            link: string,
+            bankAccount: string,
+            products: string,
+          },
+          location: {
+            map: string,
+            city: string,
+            country: string,
+          },
+          adminInfo: {
+            adminName: string,
+            adminEmail: string,
+          },
+        } */
+    const model = await new projectModel({
+      slug: req.body.slug,
+      primaryData: {
+        communityId: req.body.primaryData.communityId,
+        name: req.body.primaryData.name,
+        logo: req.body.primaryData.logo,
+        objective: req.body.primaryData.objective
+        /*         description: req.body.primaryData.description,
+                howUseDonation: req.body.primaryData.howUseDonation,
+                sponsors: req.body.primaryData.sponsors, */
+      },
+      /*         contact: {
+                email: req.body.contact.email,
+                phone: req.body.contact.phone,
+                whatsapp: req.body.contact.whatsapp,
+                instagram: req.body.contact.instagram,
+                facebook: req.body.contact.facebook,
+                linkedin: req.body.contact.linkedin,
+                twitter: req.body.contact.twitter,
+                website: req.body.contact.website,
+              }, */
+      /*        paymentData: {
+               link: req.body.paymentData.link,
+               bankAccount: req.body.paymentData.bankAccount,
+               products: req.body.paymentData.link,
+             }, */
+      location: {
+        map: req.body.location.map,
+        city: req.body.location.city,
+        country: req.body.location.country,
+      },
+      adminInfo: {
+        adminName: req.body.adminInfo.adminName,
+        adminEmail: req.body.adminInfo.adminEmail,
+      }
+    })
+    await model.save((err: any) => {
+      if (err) {
+        return res.json(err);
+      } else {
+        return res.json(model)
+      }
+    })
   },
   getForFilters: async (req: Request, res: Response) => {
     const { country, products, paymentData, transfer, community } = req.params;
--- backend/src/api/routes/projects.ts
+++ backend/src/api/routes/projects.ts
@@ -17,7 +17,6 @@ router.get('/', getAllProjects);
 router.get('/organizations', getOld);
 router.get('/pagination', getPagination);
 router.get('/pagination/page/:page', getPagination);
-// router.get('/filters/:country&:products&:paymentData&:transfer', getForFilters)
 router.get('/filters/:country&:products&:paymentData&:transfer&:community', getForFilters);
 router.get('/lastest/:quanty', getLastest);
 router.get('/distinct/', getDistinctCountry);
