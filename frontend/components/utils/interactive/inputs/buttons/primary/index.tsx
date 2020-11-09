718165f223377d3da9cef1ed2579b4e03047dc6a
urielri <urielrivps@gmail.com>
2020-11-09 14:27:35 -0300

    sdf

--- frontend/components/utils/interactive/inputs/buttons/primary/index.tsx
+++ frontend/components/utils/interactive/inputs/buttons/primary/index.tsx
@@ -1,3 +1,38 @@
-export function Button() {
-  'ads'
+function Button(props: string) {
+  const { desc, width, color, colorFont } = props
+  return (
+    <div className="containerButton">
+      <button className="button">
+        <span>{desc}</span>
+      </button>
+      <style jsx>
+        {`
+          .containerButton {
+            max-width: 350px;
+            width: ${width};
+            background: ${color};
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 48px;
+            border-radius: 5px;
+          }
+          .button {
+            width: 100%;
+            height: 100%;
+            cursor: pointer;
+          }
+          .button span {
+            color: ${colorFont ? colorFont : '#fff'};
+            font-weight: bold;
+            font-size: 1.2rem;
+          }
+          .button:active {
+            color: red;
+          }
+        `}
+      </style>
+    </div>
+  )
 }
+export default Button
--- frontend/pages/admin/testing.tsx
+++ frontend/pages/admin/testing.tsx
@@ -8,7 +8,7 @@ import {
   Select,
   Option,
 } from '../../components/utils/interactive/inputs/form/select'
-
+import Button from '@/components/utils/interactive/inputs/buttons/primary'
 function Testing() {
   const [changes, setChanges] = useState({
     area: '',
@@ -63,84 +63,18 @@ function Testing() {
         <title>Testings</title>
       </Head>
       <Container>
-        {/*
-          <Textarea
-          desc="Ingrese algo textarea"
-          onChange={(e) => handleChange(e)}
-        />
-        <Text desc="Ingrese algo inputText" onChange={(e) => handleChange(e)} />
-        <div>
-          <form onChange={(e) => handleChangeForm(e)}>
-            <Radio
-              desc="radio"
-              name="radios"
-              value="si"
-              checked={true}
-              disabled={false}
-            />
-            <Radio
-              desc="rad"
-              name="radios"
-              value="no"
-              disabled={dis}
-              checked={false}
-            />
-            <Radio
-              desc="rada"
-              name="radios"
-              value="tal vez"
-              disabled={false}
-              checked={false}
-            />
-            <Radio
-              desc="radas"
-              name="radios"
-              value="tal vez si o no"
-              disabled={false}
-              checked={false}
-            />
-            <Radio
-              desc="radass"
-              name="radios"
-              value="tal vez si si o no no "
-              disabled={false}
-              checked={false}
-            />
-          </form>
+        <div className="content">
+          <Button desc="Ingrese text" color="#237ed5" width="250px" />
+          <style jsx>
+            {`
+              .content {
+                height: 600px;
+                display: grid;
+                place-items: center;
+              }
+            `}
+          </style>
         </div>
-        <button onClick={offDisabled}>offDisabled</button>
-        <Select/>
-
-        <h2>{changes.area}</h2>
-        <h2>{changes.inp}</h2>
-        <h2>{changes.rad}</h2>
-           */}
-        <Select
-          // cambia el label que me tira error paadre
-          label="asd"
-          onChange={handleSelect}
-          value={changes.sel}
-          id="puto"
-        >
-          {(val) => (
-            <>
-              <Option desc="vamo nwelll" value="1" val={val} />
-              <Option desc="puede ser pa" value="2" val={val} />
-              <Option desc="tal vez" value="3" val={val} />
-              <Option desc="depende" value="4" val={val} />
-              <Option desc="a veces no" value="5" val={val} />
-              <Option desc="ahora si" value="6" val={val} />
-              <Option desc="antes no andaba" value="7" val={val} />
-              <Option desc="no atiendo boludos" value="8" val={val} />
-              <Option desc="y va el tercero" value="9" val={val} />
-              <Option desc="gol del pity" value="10" val={val} />
-              <Option desc="tal vez si si o no no" value="11" val={val} />
-            </>
-          )}
-        </Select>
-        <h2>
-          <span>{changes.sel}</span>
-        </h2>
       </Container>
     </>
   )
