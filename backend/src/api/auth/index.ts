2493cc0d458d8f49d685aecd59390f5dae38b0da
Matias Caballero <maticaba99@hotmail.com>
2021-02-10 18:13:50 -0300

    Delete backend directory

--- backend/src/api/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import express, { Request, Response } from 'express';
-
-const route = express.Router();
-
-route.get('/auth/csrf', async (req: Request, res: Response) => {
-  res.status(200).json({ data: { csrf: req.csrfToken() } });
-});
-
-export { route as authApi };
